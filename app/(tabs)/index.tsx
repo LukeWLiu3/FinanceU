0423cab110cc8efdc2776d3c6137540fc04b8904
LukeWLiu3 <lukewliu3@gmail.com>
2025-07-20 17:29:28 -0500

    First ver

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text className="bg-red-400">Hi example page</Text>
+    </View>
+  );
+}
+
